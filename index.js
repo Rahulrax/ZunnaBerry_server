2d23a719b62d27ddf4ffa278042b29d77558d1e3
Rahul Kumar <rahulraxstar4@gmail.com>
2025-07-27 03:24:58 +0530

    Zunnaberry backend 1st commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected successfully!"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
+
+app.get("/", (req, res) => {
+  res.json({ message: "👋 Welcome to the Zunnaberry backend API!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
